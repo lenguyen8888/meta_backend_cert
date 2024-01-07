dd4e9740ab730d9751b5fa21367f91f4744b1ded
Nguyen Le <lenguyen_d@hotmail.com>
2024-01-07 11:56:57 -0800

    Add more lab

---
+++ intro-to-databases-back-end-development/week2/exercise-practicing-table-creation/my_lab.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE football_club;
+
+USE football_club;
+
+CREATE TABLE players (playerID int, playerName varchar(50), age int);
+
+SHOW tables;
+
+-- Additional task
+
+CREATE TABLE games(gameID INT, gameDate DATE, score INT);
+
+SHOW tables;
+
